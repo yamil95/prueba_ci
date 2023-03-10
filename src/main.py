f00502c56f651664f360bd60e0ff26aaad4c78ff
Yamil Ferrufino <Yferrufino@suppliers.tenaris.com>
2023-03-10 15:46:50 -0300

    add: agregue primer template de prueba

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+
+dictionary = {"columna_1": [1,2,3,4],'columna_2':[5,6,7,8]}
+
+dataframe = pd.DataFrame(dictionary)
+
+
+
+def sumar (a:int,b:int):
+    return a + b 
+
+
+
---
+++ src/tests/test_suma.py
@@ -0,0 +1,9 @@
+from src.main  import sumar
+
+
+
+class Test_sumar ():
+
+    def test_sumar (self):
+        assert sumar(2,2) == 4
+
