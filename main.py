7de58d529d0e5eb225bcacdec1fe592a8797c9f3
Yamil <yferru.9259@gmail.com>
2023-05-02 14:00:05 -0300

    fix : modify tree directory

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import pandas as pd 
-
-
-dictionary = {"columna_1": [1,2,3,4],'columna_2':[5,6,7,8]}
-
-dataframe = pd.DataFrame(dictionary)
 
 
 
@@ -13,7 +7,7 @@ def sumar (a:int,b:int):
 def restar (a:int,b:int):
     return a - b 
 
-if '__name__' == "__main__":
+if __name__ == '__main__' :
     
     print (f"la resta de 2 - 1 es : {restar(2,1)}")
 
--- src/tests/test_resta.py
+++ src/tests/test_resta.py
@@ -1,4 +1,4 @@
-from src.main  import restar
+from main  import restar
 
 
 
